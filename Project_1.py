9073401863397ca974ce10f79434fef4fd6990da
VishwajeetMitra <143841808+VishwajeetMitra@users.noreply.github.com>
2024-07-08 20:09:13 +0530

    Update Project_1.py
    Made the code more efficient by reducing the number of iterations and unnecessary imports and variables

--- Project_1.py
+++ Project_1.py
@@ -1,77 +1,27 @@
-# Import the libraries
-
-from tkinter import filedialog
-from os import listdir
-from os.path import isfile, join
 import os
 import shutil
+from tkinter import filedialog
 
 # Obtain the path to be organized
-
 file_path = filedialog.askdirectory()
 
-# Obtain all the files from the path in list
-
-files = [f for f in listdir(file_path) if isfile(join(file_path, f))]
-
-# Create the blank list and dictionary
-
+# Create a dictionary to store file types and their corresponding folders
 filetype_dict = {}
 
-
-
-# Create a loop
-
-for file in files:
-
-	# Split the name of file from dot
-
-	filetype = file.split('.')[-1]
-	
-	# Give naming to the newly created folders
-	
-	new_folder_name = file_path+'/' + filetype + '_files'
-
-	# Add the new folder name in dictionary with the key value pairs
-
-	filetype_dict[str(filetype)] = str(new_folder_name)
-
-	# Check if the folder exists or not
-
-	if os.path.isdir(new_folder_name) == True:
-
-	# Come out of the loop if folder exists
-
-		continue
-	else:
-
-	# Create the new folder if does not exist
-
-		os.mkdir(new_folder_name)
-
-
-# Create the loop for all the files
-
-for file in files:
-
-	# Get the source path of each file
-
-	src_path = file_path+'/'+file
-
-	# Split the name of file by dot
-
-	filetype = file.split('.')[-1]
-
-	# Check if the file type exists in the dictionary
-
-	if filetype in filetype_dict.keys():
-
-		# Add the file type in dictionary if not already there
-
-		dest_path = filetype_dict[str(filetype)]
-
-		# Move the file from source path to destination path
-
-		shutil.move(src_path, dest_path)
-
-	print('.',src_path + '>>>' + dest_path)
+# Iterate over all files in the directory
+for file in os.listdir(file_path):
+    # Check if the file is a regular file (not a directory)
+    if os.path.isfile(os.path.join(file_path, file)):
+        # Get the file type (extension)
+        filetype = file.split('.')[-1]
+        
+        # Create a new folder for the file type if it doesn't exist
+        new_folder_name = os.path.join(file_path, f"{filetype}_files")
+        if not os.path.isdir(new_folder_name):
+            os.mkdir(new_folder_name)
+        
+        # Move the file to its corresponding folder
+        src_path = os.path.join(file_path, file)
+        dest_path = new_folder_name
+        shutil.move(src_path, dest_path)
+        print(f"{src_path} >> {dest_path}")
